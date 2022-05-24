7827358a4fb21c794edd67b57f34537421293e91
867380699 <867380699@qq.com>
2022-05-25 07:30:21 +0800

    build: bundle analyse

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import analyze from 'rollup-plugin-analyzer';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { fileURLToPath } from 'url';
@@ -8,6 +9,16 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      plugins: [
+        analyze({
+          summaryOnly: true,
+          limit: 20,
+        }),
+      ],
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
@@ -36,6 +47,7 @@ export default defineConfig({
     },
   },
   server: {
+    https: true,
     proxy: {
       '^/rss': {
         target: 'http://localhost',
