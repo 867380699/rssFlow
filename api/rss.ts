3dd1d66319965ebc52cb6dca3429ed12bc1efb0a
867380699 <867380699@qq.com>
2022-08-28 16:07:09 +0800

    fix(vercel): ts config

--- api/rss.ts
+++ api/rss.ts
@@ -5,7 +5,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const { url, name } = request.query;
   console.log(request, response);
   if (url) {
-    const resp = await fetch(decodeURIComponent(origin));
+    const resp = await fetch(decodeURIComponent(url.toString()));
     return response.status(200).setHeader('Access-Control-Allow-Origin', '*').send(resp.body);
   }
   return response.status(200).setHeader('Access-Control-Allow-Origin', '*').send(`Hello ${name}!`);
