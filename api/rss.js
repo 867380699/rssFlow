cdde201e00e372466555198d5bc86ba1ace105d9
867380699 <867380699@qq.com>
2022-08-28 14:47:24 +0800

    fix(vercel): servless api test again

--- api/rss.js
+++ api/rss.js
@@ -6,7 +6,7 @@ export default function handler(request, response) {
   if (url) {
     const resp = await fetch(decodeURIComponent(origin));
     const newResp = new Response(resp.body, resp);
-    newResp.headers.set('Access-Control-Allow-Origin', '*');
+    newResp.setHeader('Access-Control-Allow-Origin', '*');
     return newResp; 
   }
   return response.status(200).setHeader('Access-Control-Allow-Origin', '*').send(`Hello ${name}!`);
