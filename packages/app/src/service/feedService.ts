bc1a1449d1b6a82f6c82420ff51df739272f507d
867380699 <867380699@qq.com>
2022-11-08 16:32:36 +0800

    feat(parser): atom feed support

--- packages/app/src/service/feedService.ts
+++ packages/app/src/service/feedService.ts
@@ -13,7 +13,63 @@ import { feedDB, storeFeedItems } from './dbService';
 
 const parser = new DOMParser();
 
-const parseFeedItems = (nodeTree: Document): Array<FeedItem> => {
+export const parseFeed = (feed: string, source: string): Feed => {
+  const nodeTree = parser.parseFromString(feed, 'text/xml');
+  if (nodeTree.querySelector('feed')) {
+    return parseAtomFeed(nodeTree, source);
+  } else {
+    return parseRSSFeed(nodeTree, source);
+  }
+};
+
+const parseRSSFeed = (nodeTree: Document, source: string): Feed => {
+  const title =
+    nodeTree.querySelector('rss > channel > title')?.textContent || '';
+  const description =
+    nodeTree.querySelector('rss > channel > description')?.textContent || '';
+  const link =
+    nodeTree.querySelector('rss > channel > link')?.textContent || '';
+  const imageUrl =
+    nodeTree
+      .querySelector('rss > channel > image > url')
+      ?.textContent?.replace(/^https?/, 'https') || '';
+  const items = parseRSSFeedItems(nodeTree);
+  return {
+    type: 'feed',
+    parentId: 0,
+    source,
+    title,
+    description,
+    link,
+    imageUrl,
+    items,
+  };
+};
+
+const parseAtomFeed = (nodeTree: Document, source: string): Feed => {
+  const title = nodeTree.querySelector('feed > title')?.textContent || '';
+  const description =
+    nodeTree.querySelector('feed > subtitle')?.textContent || '';
+  const link =
+    nodeTree.querySelector('feed > link')?.getAttribute('href') || '';
+  const imageUrl =
+    nodeTree
+      .querySelector('feed > image > url')
+      ?.textContent?.replace(/^https?/, 'https') || '';
+  const items = parseAtomFeedItems(nodeTree);
+  return {
+    type: 'feed',
+    parentId: 0,
+    source,
+    title,
+    description,
+    link,
+    imageUrl,
+    items,
+  };
+};
+
+const parseRSSFeedItems = (nodeTree: Document): Array<FeedItem> => {
   const itemNodes = nodeTree.querySelectorAll('rss > channel > item');
   const items: Array<FeedItem> = [];
   itemNodes.forEach((node) => {
@@ -37,29 +93,30 @@ const parseFeedItems = (nodeTree: Document): Array<FeedItem> => {
   return items;
 };
 
-export const parseFeed = (feed: string, source: string): Feed => {
-  const nodeTree = parser.parseFromString(feed, 'text/xml');
-  const title =
-    nodeTree.querySelector('rss > channel > title')?.textContent || '';
-  const description =
-    nodeTree.querySelector('rss > channel > description')?.textContent || '';
-  const link =
-    nodeTree.querySelector('rss > channel > link')?.textContent || '';
-  const imageUrl =
-    nodeTree
-      .querySelector('rss > channel > image > url')
-      ?.textContent?.replace(/^https?/, 'https') || '';
-  const items = parseFeedItems(nodeTree);
-  return {
-    type: 'feed',
-    parentId: 0,
-    source,
-    title,
-    description,
-    link,
-    imageUrl,
-    items,
-  };
+const parseAtomFeedItems = (nodeTree: Document): Array<FeedItem> => {
+  const itemNodes = nodeTree.querySelectorAll('feed > entry');
+  const items: Array<FeedItem> = [];
+  itemNodes.forEach((node) => {
+    const description = node.querySelector('content')?.textContent || '';
+
+    const contentDocument = parser.parseFromString(description, 'text/html');
+    const image = contentDocument.querySelector('img')?.src;
+    const pubDate = node.querySelector('updated')?.textContent;
+
+    items.push({
+      title: node.querySelector('title')?.textContent || '',
+      image,
+      shortDescription: (contentDocument.body.textContent || '').trim(),
+      description,
+      link:
+        node
+          .querySelector('link')
+          ?.getAttribute('href')
+          ?.replace(/^https?/, 'https') || '',
+      pubDate: (pubDate ? new Date(pubDate) : new Date()).getTime(),
+    });
+  });
+  return items;
 };
 
 export const parseFeedContent = time((content: string) => {
@@ -110,10 +167,8 @@ const buildVNode = (e: HTMLElement, scope: any) => {
   let component: any = e.tagName.toLocaleLowerCase();
 
   if (e.tagName === 'BODY') {
-    console.log('un body', scope.imageCount);
-
     component = LazyFeedContent;
-    props.imgs = scope.imgs;
+    props.imgs = scope.imgs || [];
     props.offset = 2;
   } else if (e.tagName === 'IMG') {
     const { minHeight } = useMinHeight();
