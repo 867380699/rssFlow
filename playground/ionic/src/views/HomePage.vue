602d0fea6f4819d70287f30705d0ff76b22dffb0
cm1 <867380699@qq.com>
2023-07-07 12:39:01 +0800

    feat(playground): ionic init

---
+++ playground/ionic/src/views/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Ionic</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-list>
+        <ion-item>
+          Browser
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonItem,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+
+</script>
