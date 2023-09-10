7c6bb84dd561f31d0326b6afcf17071bfea28ecf
cm1 <867380699@qq.com>
2023-09-11 01:17:33 +0800

    fix(vercel): api image proxy

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,15 +1,19 @@
-import fetch from 'node-fetch';
 import { VercelRequest, VercelResponse } from '@vercel/node';
-
+import fetch from 'node-fetch';
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const origin = (request.query.origin || '').toString();
   console.log(origin);
   if (/^https?:\/\/.*/.test(origin)) {
     const resp = await fetch(origin.toString());
-    const data = await resp.text();
-    // console.log(data);
-    response.status(200).send(data);
+
+    for (let [k, v] of Object.entries(resp.headers)){
+      if (v) {
+        response.setHeader(k, v);
+      }
+    }
+
+    resp.body?.pipe(response);
   } else {
     response.status(404).send('404');
   }
